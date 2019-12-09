787c70e013320afed2ed292dddd9b87c41432874
Puune <43564655+Puune@users.noreply.github.com>
2019-12-09 10:39:21 +0200

    uibuild

--- ep-backend/utils/config.js
+++ ep-backend/utils/config.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 let MONGODB_URI;
 if(process.env.NODE_ENV === 'test'){
   MONGODB_URI = process.env.MONGODB_URI_TEST;
+} else if(process.env.NODE_ENV === 'heroku'){
+  MONGODB_URI = 'mongodb+srv://fullstack:verySecure@cluster0-d8aqo.mongodb.net/event-planner?retryWrites=true&w=majority'
 } else {
   MONGODB_URI = process.env.MONGODB_URI;
 }
